76d3fbec3a3a64e7ddb09c1a6207f81ba5c8e1c6
Priya Patil <priya10487@aol.com>
2015-06-23 14:28:01 +0100

    heapifys array of 3 or less

--- lib/heapsort.rb
+++ lib/heapsort.rb
@@ -1,27 +1,25 @@
 class Heapsort 
 
     def heapsort(array)
-        if array.size < 1 
+        if array.size <= 1 
             return array
         end
     end
 
     def heapify(array,i)
         array.each do |sort|
-            i = (i.to_i - 1 /2).floor
+            i = (i/2).floor
             left_index  = (2*i + 1)
             right_index = (2*i + 2) 
             if left_index <= array.length - 1 && array[left_index] > array[i]
                 largest = left_index
             else largest = i
             end
-            if right_index <= array.length - 1 && array[i] > array[largest]
+            if right_index <= array.length - 1 && array[right_index] > array[largest]
                 largest = right_index
             end
-            if largest != array[i]
-                temp = array[i]
-                array[i] = largest
-                largest = temp
+            if array[largest] != array[i]
+                array[i], array[largest] = array[largest], array[i]
                 heapify(array,largest)
             end
         end
--- spec/heapsort_spec.rb
+++ spec/heapsort_spec.rb
@@ -1,17 +1,17 @@
 require "heapsort"
 
 describe '#heap_sort' do
-    it 'sorts an empty array' do 
+   it 'sorts an empty array' do 
         expect(Heapsort.new.heapsort([])).to eq([])
     end
     it 'heapify builds a heap of 1' do
-        expect(Heapsort.new.heapify([1],1)).to eq([1])
+        expect(Heapsort.new.heapsort([1])).to eq([1])
     end
-    it 'heapify returns a reorderd array of 2' do
-        expect(Heapsort.new.heapify([1,2],1)).to eq([1,2])
+    it 'heapify returns a reorderd array of 3' do
+        expect(Heapsort.new.heapify([1,2,3],1)).to eq([3,2,1])
     end
-    it 'heapify returns the index of an element in an array' do
-        expect(Heapsort.new.heapify([20,1,3],1)).to eq([20,1,3])
+    it 'heapify returns an ordered  array' do
+        expect(Heapsort.new.heapify([1,3,20],1)).to eq([20,3,1])
     end
 end
 
